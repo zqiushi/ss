c2d5a2cc5a0c8ecccd94912c0307e122cb2fae13
zqiushi <zqiushi@yeah.net>
2019-12-10 18:14:30 +0800

    add docker-compose

--- init.sh
+++ init.sh
@@ -3,14 +3,14 @@
 
 if [ ! -f .env ]; then
  cp .env_bak .env        #copy .envconfig file
- echo "docker-compose -f /opt/docker-compose.yaml up -d" >> /etc/rc.d/rc.local
+ echo "/usr/local/bin/docker-compose -f /opt/docker-compose.yaml up -d" >> /etc/rc.d/rc.local
  ln -s docker-compose.yaml /opt/docker-compose.yaml
  yum install -y docker docker-compose
 fi
 
 #stop service
-docker-compose -f /opt/docker-compose.yaml down
+/usr/local/bin/docker-compose -f /opt/docker-compose.yaml down
 
 #start service
-docker-compose -f /opt/docker-compose.yaml up -d
+/usr/local/bin/docker-compose -f /opt/docker-compose.yaml up -d
 
