ce211f527c5e2c37f2b83eab0d836e309145d571
zqiushi <zqiushi@yeah.net>
2019-12-10 18:09:06 +0800

    add install docker-compose and run when os starup

--- init.sh
+++ init.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+
 if [ ! -f .env ]; then
  cp .env_bak .env        #copy .envconfig file
+ echo "docker-compose -f /opt/docker-compose.yaml up -d" >> /etc/rc.d/rc.local
+ ln -s docker-compose.yaml /opt/docker-compose.yaml
+ yum install -y docker docker-compose
 fi
 
 #stop service
-docker-compose -f docker-compose.yaml down
+docker-compose -f /opt/docker-compose.yaml down
 
 #start service
-docker-compose -f docker-compose.yaml up -d
+docker-compose -f /opt/docker-compose.yaml up -d
 
