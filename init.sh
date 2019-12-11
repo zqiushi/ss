694a1fd1e8c8e5a53f897f043e4206ae53e2a916
yangbo <zqiushi@yeah.net>
2019-12-11 14:14:17 +0800

    use cp  note ln docker-conpose.yaml

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 if [ ! -f .env ]; then
  cp .env_bak .env        #copy .envconfig file
  yum install -y epel-release  docker-ce  docker-compose
- ln -s docker-compose.yaml /opt/docker-compose.yaml
+ cp docker-compose.yaml /opt/docker-compose.yaml
  echo "/usr/bin/docker-compose -f /opt/docker-compose.yaml up -d" >> /etc/rc.d/rc.local
 fi
 
